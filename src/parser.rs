619f93137a608b7afae807ce7b40c99fd3a9223e
Jason <jason.ang2005@gmail.com>
2024-01-22 15:30:47 +0700

    made simple test for parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,6 @@
 use super::lexer;
 
+#[derive(Debug, PartialEq)]
 pub struct Ast {
     token: lexer::Token,
     children: Vec<Ast>,
@@ -8,5 +9,25 @@ pub struct Ast {
 impl Ast {
     pub fn new(tokens: lexer::TokenStream) -> Ast {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::lexer::Token;
+
+    use super::*;
+
+    #[test]
+    fn parse_simple() {
+        let tokens = lexer::TokenStream::new(";play amogus");
+        let expected_ast = Ast {
+            token: Token::Command("play".to_string()),
+            children: vec![Ast {
+                token: Token::Argument("amogus".to_string()),
+                children: vec![],
+            }],
+        };
+        assert_eq!(Ast::new(tokens), expected_ast);
     }
 }
