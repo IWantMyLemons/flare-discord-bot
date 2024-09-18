318fd649296e0ff30277f96bbdbb1dbd6e15a33f
citrusfur <citrusfur@proton.me>
2024-09-19 01:54:53 +0700

    TIME TO START FROM SCRATCH

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,11 @@
-pub mod lexer;
-pub mod parser;
-
 use std::env;
 
-use serenity::{
-    all::{Message, Ready},
-    async_trait,
-    prelude::*,
-};
-
-struct Handler;
-
-#[async_trait]
-impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
-        if !msg.content.starts_with(';') {
-            return;
-        }
-
-        let tokens = lexer::TokenStream::new(msg.content.as_str());
-        let _ast = parser::Ast::vec_new(tokens);
-
-        if msg.content.eq_ignore_ascii_case(";ping") {
-            if let Err(e) = msg.channel_id.say(&ctx.http, "Pong :3").await {
-                println!("Error sending message: {e}");
-            }
-        }
-    }
-    async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected! :D", ready.user.name);
-    }
-}
+use serenity::prelude::*;
 
 #[tokio::main]
 async fn main() {
     // Configure client with discord token
-    let token = env::var("DISCORD_TOKEN").expect("Discord token not found >:(");
+    let token = env::var("DISCORD_TOKEN").expect("Discord token not found");
 
     // Setup gateway intents
     let intents = GatewayIntents::GUILD_MESSAGES
@@ -44,7 +14,6 @@ async fn main() {
 
     // Create an instance of the Client
     let mut client = Client::builder(token, intents)
-        .event_handler(Handler)
         .await
         .expect("Failed to create client :(");
 
