56876836b4063fb392f95fdb8078799ef51c5123
citrus <81078642+IWantMyLemons@users.noreply.github.com>
2025-09-17 22:08:31 +0700

    fixed macro to using the right identifier

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,7 +1,7 @@
 use proc_macro::TokenStream;
 use quote::{ToTokens, quote};
 use syn::{
-    Expr, FnArg, ItemFn, Lit, Type, parse_macro_input,
+    parse_macro_input, Expr, FnArg, Ident, ItemFn, Lit, Pat, Type
 };
 
 #[proc_macro_attribute]
@@ -20,18 +20,20 @@ pub fn command(_attr: TokenStream, item: TokenStream) -> TokenStream {
     });
 
     // ensure first argument is a PrefixContext, changing the first argument if necessary
-    let context_ident = if let Some(arg) = function.sig.inputs.first()
+    let _context_ident: Ident = if let Some(arg) = function.sig.inputs.first()
         && let FnArg::Typed(pat_type) = arg
         && let Type::Path(type_path) = pat_type.ty.as_ref()
-        && let Some(last_segment) = type_path.path.segments.last()
-        && last_segment.ident == "PrefixContext"
+        && type_path.path.is_ident("PrefixContext")
     {
-        last_segment.ident.clone()
+        match pat_type.pat.as_ref() {
+            Pat::Ident(pat_ident) => {pat_ident.ident.clone()}
+            _ => { panic!("No identifier found") }
+        } 
     } else {
         let arg = syn::parse(quote! {__context: framework::structs::prefix::PrefixContext<'_>}.into())
             .unwrap();
         function.sig.inputs.insert(0, arg);
-        syn::parse(quote! {__context}.into()).unwrap()
+        syn::parse::<Ident>(quote! {__context}.into()).unwrap()
     };
 
     let arguments = &function.sig.inputs.iter().collect::<Vec<_>>();
