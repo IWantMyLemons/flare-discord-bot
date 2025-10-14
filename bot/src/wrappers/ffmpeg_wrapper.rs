770a211a0336a5b52591e69e2f159f922f200d75
citrus <81078642+IWantMyLemons@users.noreply.github.com>
2025-10-14 15:41:11 +0700

    fixed transparency

--- bot/src/wrappers/ffmpeg_wrapper.rs
+++ bot/src/wrappers/ffmpeg_wrapper.rs
@@ -6,7 +6,15 @@ use tokio::{
 
 pub async fn minify_gif(bytes: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut ffmpeg = Command::new("ffmpeg")
-        .args(["-i", "pipe:0", "-f", "gif", "-vf", "scale=32:32", "pipe:1"])
+        .args([
+            "-i",
+            "pipe:0",
+            "-f",
+            "gif",
+            "-vf",
+            "scale=32:32,split[s1][s2],[s1]palettegen[p],[s2][p]paletteuse",
+            "pipe:1",
+        ])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
@@ -20,7 +28,7 @@ pub async fn minify_gif(bytes: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
     let mut stdout = ffmpeg.stdout.take().expect("Failed to open stdout");
     let mut output: Vec<u8> = Vec::new();
     stdout.read_to_end(&mut output).await?;
-    
+
     ffmpeg.wait().await?; // wait for ffmpeg to actually close
 
     Ok(output)
