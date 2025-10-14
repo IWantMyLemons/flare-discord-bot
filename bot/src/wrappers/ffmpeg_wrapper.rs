11eeefa614416dd014e0a842317252aca408bfea
citrus <81078642+IWantMyLemons@users.noreply.github.com>
2025-10-14 13:57:44 +0700

    waiting for ffmpeg now

--- bot/src/wrappers/ffmpeg_wrapper.rs
+++ bot/src/wrappers/ffmpeg_wrapper.rs
@@ -5,21 +5,23 @@ use tokio::{
 };
 
 pub async fn minify_gif(bytes: Vec<u8>) -> Result<Vec<u8>, Box<dyn Error>> {
-    let ffmpeg = Command::new("ffmpeg")
-        .args(["-i", "pipe:0", "-f", "gif", "-r", "1", "pipe:1"])
+    let mut ffmpeg = Command::new("ffmpeg")
+        .args(["-i", "pipe:0", "-f", "gif", "-vf", "scale=32:32", "pipe:1"])
         .stdin(Stdio::piped())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
         .spawn()?;
 
     {
-        let mut stdin = ffmpeg.stdin.expect("Failed to open stdin");
+        let mut stdin = ffmpeg.stdin.take().expect("Failed to open stdin");
         stdin.write_all(&bytes).await?;
     }
 
-    let mut stdout = ffmpeg.stdout.expect("Failed to open stdout");
+    let mut stdout = ffmpeg.stdout.take().expect("Failed to open stdout");
     let mut output: Vec<u8> = Vec::new();
     stdout.read_to_end(&mut output).await?;
+    
+    ffmpeg.wait().await?; // wait for ffmpeg to actually close
 
     Ok(output)
 }
